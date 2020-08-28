393e2a4b82a43e1bed999ecbaced0612f62e8a28
syedarhamali <arham426@gmail.com>
2020-08-28 08:35:41 +0500

    fix media Queries and update style

--- app.js
+++ app.js
@@ -1,46 +1,59 @@
-var list = document.getElementById("list")
+var table_input = document.getElementById('table-input');
 
-function addTodo() {
+function insertTodo(){
+    //   get Input from User and insert it into table data tag
+    var todo_input = document.getElementById('todo-input');
+    if(todo_input.value === ""){
+        alert("Please Write Something ....")
+    }
+    else{
+        var table_item = document.createElement('td');
+        table_item.setAttribute("class","table_data");
+        var table_Text = document.createTextNode(todo_input.value);
+        table_item.appendChild(table_Text);
+    }
     
-    // add todo button and get values
-    var todo_item = document.getElementById("todo-item");
-    var li = document.createElement("li")
-    li.setAttribute("class","list_items")
-    var liText = document.createTextNode(todo_item.value)
-    li.appendChild(liText)
+    // create delete button and insert it into table data tag
+    var DeleteBtn = document.createElement("button");
+    var DeleteBtnText = document.createTextNode("Delete");
+    DeleteBtn.setAttribute("class","DeleteBtn")
+    DeleteBtn.setAttribute("onclick","deleteItems(this)")
+    DeleteBtn.appendChild(DeleteBtnText);
+    var table_2 = document.createElement('td');
+    table_2.appendChild(DeleteBtn);
     
+        //  create edit button and insert it into table tag
+    var editBtn = document.createElement("button");
+    var editBtnText = document.createTextNode("Edit");
+    editBtn.setAttribute("class","editBtn");
+    editBtn.setAttribute("onclick","editItems(this)");
+    editBtn.appendChild(editBtnText);
+    var table_3 = document.createElement('td');
+    table_3.appendChild(editBtn);
 
-    //    edit button and values
-    var editbtn = document.createElement("button")
-    var editTxt = document.createTextNode("Edit")
-    editbtn.appendChild(editTxt)
-    editbtn.setAttribute("class","edit_btn")
-    editbtn.setAttribute("onclick","editItem(this)")
-    li.appendChild(editbtn)
-    
-    // add delete button and get values  
-    var dltbtn = document.createElement("button")
-    var dltText = document.createTextNode("Delete")
-    dltbtn.setAttribute("class","delete_btn")
-    dltbtn.setAttribute("onclick","deleteitem(this)")
-    dltbtn.appendChild(dltText)
-    li.appendChild(dltbtn)
-    
-    list.appendChild(li)
+    //    make table row and insert user input , delete button and edit button in it 
+    var table = document.createElement('tr');
+    table.appendChild(table_item);
+    table.appendChild(table_2);
+    table.appendChild(table_3);
 
-    todo_item.value = ""
-
+    // now add this table row and colomns to table 
+    table_input.appendChild(table);
+    todo_input.value = ""
 }
 
-function deleteitem(a) {
-    a.parentNode.remove()
-}
-
+//   remove all elements 
 function deleteall(){
-    list.innerHTML = ""
+    table_input.innerHTML = ""
 }
-
-function editItem(a){
-    var editValue = prompt("Edit your Value",a.parentNode.firstChild.nodeValue)
-    a.parentNode.firstChild.nodeValue = editValue
+ 
+//    delete specific element 
+function deleteItems(para){
+    para.parentNode.parentNode.remove();
+}
+ 
+//   edit specific element 
+function editItems(para){
+    var modifiedValue = prompt("write your value",para.parentNode.parentNode.firstChild.firstChild.nodeValue);
+    para.parentNode.parentNode.firstChild.firstChild.nodeValue = modifiedValue
 }
